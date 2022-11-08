b8533b2b4415cab3bcaf73387899ee484d8e0da1
w68147 <valerahristosenko@gmail.com>
2022-11-08 17:01:41 +0100

    lab 4

---
+++ lab3/lab 4.py
@@ -0,0 +1,6 @@
+a = float(input("write"))
+b = float(input("write"))
+if a == b:
+    print (True)
+if a != b:
+    print (False)
---
+++ pythonProject/lab 4.1.py
@@ -0,0 +1,6 @@
+lista_zakupy = []
+for i in range(4):
+    lista_zakupy.append(input())
+print(lista_zakupy[0: 2])
+lista_zakupy[2] = "mleko"
+print(lista_zakupy[2:])
