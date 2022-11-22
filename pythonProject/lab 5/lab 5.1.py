11301b19edfeb3b19dcaa9b8c9cd3f883bfcd48f
w68147 <valerahristosenko@gmail.com>
2022-11-22 17:00:09 +0100

    lab 4

--- pythonProject/lab 5/lab 5.1.py
+++ pythonProject/lab 5/lab 5.1.py
@@ -1,5 +1,20 @@
 a1 = [10, 20, 30]
 a2 = ["ten", "twenty", "thirty"]
-for x in random(3):
-    d = dict(zip(str(a2[x], a1[y])))
-print(x)
+#D1 = dict(zip(a1, a2))
+#print(D1)
+#print(list(zip(a1, a2)))
+D1={}
+for i in range (len(a2)):
+    D1[a2[i]]=a1[i]
+print(D1)
+D1={a2[i]:a1[i] for i in range (len(a2))}
+print(D1)
+
+D2 = dict(thirty=30,forty=40, fifty=50)
+print (D2)
+
+D3={}
+D3.update(D1)
+D3.update(D2)
+print(D3)
+
---
+++ pythonProject/lab4.2/5.3.py
@@ -0,0 +1,13 @@
+x = 0
+y = 0
+a = []
+for i in range(15):
+    x += 1
+    y = x**2
+    a.append([x, y])
+for i in a:
+    print(i)
+
+
+
+
